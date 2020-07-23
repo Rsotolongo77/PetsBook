4f380bd9b309ea363e6ef4666f7e74c122321511
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-07-23 18:05:04 -0400

    added firebase app config for token authentication.

--- PetsBook-functions/functions/index.js
+++ PetsBook-functions/functions/index.js
@@ -1,13 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyC4oyRWNQNjSGuQ5i5T4z7bG7mqHasZuQ0",
+    authDomain: "petsbook-fc7cc.firebaseapp.com",
+    databaseURL: "https://petsbook-fc7cc.firebaseio.com",
+    projectId: "petsbook-fc7cc",
+    storageBucket: "petsbook-fc7cc.appspot.com",
+    messagingSenderId: "415177281792",
+    appId: "1:415177281792:web:e17305295286b7af93a3e5",
+    measurementId: "G-L3XRC77DWG"
+};
+
+const app = require('express')();
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
 app.get('/yelps', (req, res) => {
-    admin.firestore().collection('Yelps').get()
+    admin.firestore()
+        .collection('Yelps')
+        .orderBy('createdAt', 'desc')
+        .get()
         .then(data => {
             let yelps = [];
             data.forEach(doc => {
@@ -27,7 +43,7 @@ app.post('/yelp', (req, res) => {
     const newYelp = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin.firestore()
